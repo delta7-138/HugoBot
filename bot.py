3030e2760d63e12390de773c7867a7959af7ce26
delta7-138 <krishna.sharma@iiitb.org>
2021-01-16 12:52:52 +0530

    added Mars rover curiosity pic functionality and random name functionality

--- bot.py
+++ bot.py
@@ -1,65 +1,67 @@
 import discord
 import os
 import random
+import requests
+import lyricsgenius
 from dotenv import load_dotenv
+from discord.ext import commands
 from PIL import Image
-
+from listofnames import first_names,last_names
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-client = discord.Client()
-
+API_TOKEN = os.getenv('API_TOKEN')
+client = commands.Bot(command_prefix = '+')
+client.remove_command('help')
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.command()
+async def help(ctx):
+    embed = discord.Embed(title = 'Hugo Help' , color=0x00ffea)
+    embed.add_field(name = "Command to greet Hugo" , value = "+hello" , inline = False)
+    embed.add_field(name = "Command to generate a random color" , value = "+color" , inline = False)
+    embed.add_field(name = "Command to generate random name" , value = "+randomname" , inline = False)
+    embed.add_field(name = "Command to get MARS rover(Curiosity)" , value = "+mars ``sol_number``" , inline = False)
+    await ctx.send(embed = embed)
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@client.command()
+async def hello(ctx): 
+    await ctx.send(f'Hello There!')
 
-    if message.content.startswith('$randommsg'):
-        channel = discord.utils.get(message.guild.text_channels, name="general")
-        messages = await channel.history(limit=1000).flatten()
-        if(len(messages)!=0):
-            while(True):
-                ind = random.randint(0 , len(messages))
-                if(messages[ind].author.bot):
-                    continue
-                elif(messages[ind].content=='$random'):
-                    continue
-                else:
-                    embed=discord.Embed(title='Jump', url='https://discord.com/channels/' + str(messages[ind].guild.id) + "/" + str(messages[ind].channel.id) + "/" + str(messages[ind].id), description=messages[ind].content)
-                    embed.set_author(name=messages[ind].author.name, icon_url=messages[ind].author.avatar_url)
-                    if(messages[ind].attachments):
-                        embed.set_image(url = messages[ind].attachments[0].url)
-                    await message.channel.send(embed = embed)
-                    break
-
-    if message.content=='$help':
-        embed = discord.Embed(title = 'Hugo Help' , color=0x00ffea)
-        embed.add_field(name = "Command to greet Hugo" , value = "$hello" , inline = False)
-        embed.add_field(name = "Command to print out random message" , value = "$randommsg" , inline = False)
-        embed.add_field(name = "Command to generate a random color" , value = "$color" , inline = False)
-        await message.channel.send(embed = embed)
-
-    if message.content=='$color':
-        hex_number_string = '#'
-        for i in range(6):
-            randnum = random.randint(0 , 15)
-            hexstr = str(hex(randnum)).upper()
-            hexelem = hexstr[2:]
-            hex_number_string = hex_number_string + hexelem
+@client.command()
+async def color(ctx):
+    hex_number_string = '#'
+    for i in range(6):
+        randnum = random.randint(0 , 15)
+        hexstr = str(hex(randnum)).upper()
+        hexelem = hexstr[2:]
+        hex_number_string = hex_number_string + hexelem
         
-        hex_int = int(hex_number_string[1:] , 16)
-        im = Image.new("RGB" , (100 , 100) , hex_number_string)
-        im.save('randcolor.png')
-        file = discord.File('randcolor.png')
-        embed = discord.Embed(title = 'Random Color' , color = hex_int)
-        embed.add_field(name = "Color" , value = hex_number_string)
-        embed.set_image(url = 'attachment://randcolor.png')
-        await message.channel.send(file = file , embed = embed)
+    hex_int = int(hex_number_string[1:] , 16)
+    im = Image.new("RGB" , (100 , 100) , hex_number_string)
+    im.save('randcolor.png')
+    file = discord.File('randcolor.png')
+    embed = discord.Embed(title = 'Random Color' , color = hex_int)
+    embed.add_field(name = "Color" , value = hex_number_string)
+    embed.set_image(url = 'attachment://randcolor.png')
+    await ctx.send(file = file , embed = embed) 
+
+@client.command(aliases = ['rand'])
+async def randomname(ctx):
+    f_ind = random.randint(0 , len(first_names)-1)
+    l_ind = random.randint(0 , len(last_names)-1)
+    random.shuffle(first_names)
+    random.shuffle(last_names)
+    await ctx.send(first_names[f_ind] + " " + last_names[l_ind])
+
+@client.command()
+async def mars(ctx , * , sol):
+    try:
+        gres = requests.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=' + sol + '&api_key=' + API_TOKEN)
+        gdata = gres.json()
+        await ctx.send(gdata['photos'][0]['img_src'])
+    except: 
+        await ctx.send('Sol number out of range :pensive:')
 
 client.run(TOKEN)
