64b2c4a341ea2c2e01db68462c96d59cb929d039
delta7-138 <krishna.sharma@iiitb.org>
2021-04-03 01:12:37 +0530

    high

--- bot.py
+++ bot.py
@@ -624,7 +624,7 @@ async def advice(ctx):
     adviceMsg = content["slip"]["advice"]
     embed = discord.Embed(title = "Advice for stupid people" , description = adviceMsg , color = 0x00ffea)
     embed.set_thumbnail(url = 'https://bloximages.newyork1.vip.townnews.com/omaha.com/content/tncms/assets/v3/editorial/e/84/e845347d-fc1d-53a8-9775-f2f1df4ec42a/5e98d4681f24c.image.jpg?resize=1200%2C1200')
-    embed.set_footer(text = 'requested by **' + uname + '**')
+    embed.set_footer(text = 'requested by ' + uname)
     await ctx.send(embed = embed)
 
 @client.command(aliases = ['aniq'])
@@ -635,7 +635,7 @@ async def animequote(ctx):
     character = content["character"]
     anime = content["anime"]
     embed = discord.Embed(title = "Anime Quote" , description = '*' + quote + '*'  , color = 0x00ffea)
-    embed.set_footer(text = 'by - **' + character + '**\n from - **' + anime + '**' )
+    embed.set_footer(text = 'by - ' + character + '\n from - ' + anime)
     await ctx.send(embed = embed)
     
 @client.command(aliases = ['inv'])
