d5565ee1b803fecb7d7b22759a8bdf5a6770023e
delta7-138 <krishna.sharma@iiitb.org>
2021-04-06 14:52:34 +0530

    teleport api added

--- bot.py
+++ bot.py
@@ -683,6 +683,28 @@ async def urmomiserror(ctx , err):
         mention = ctx.message.author.mention
         await ctx.send(mention + " " + joke)
 
+@client.command(aliases = ['sol'])
+async def standardofliving(ctx , * , args):
+    queryCity = urllib.parse.urlencode(args)
+    res1 = requests.get('https://api.teleport.org/api/cities/?search=' + queryCity)
+    content1 = json.loads(res1)
+    nextUrl = content1['_embedded']['city:search:results'][0]['_links']['city:item'])['href']
+    if(nextUrl):
+        res2 = requests.get(nextUrl)
+        content2 = json.loads(res2)
+        nextnextUrl = content2['_links']['city:urban_area']['href']
+        res3 = requests.get(nextnextUrl)
+        content3 = requests.get(res3)
+        city_score = content3['teleportscore']
+        embed = discord.Embed(name = 'Teleport City Summary' , color = 0x00ffea)
+        for i in content3['categories']:
+            embed.add_field(name = i['name'] , value = i['score_out_of_10'])
+
+        embed.set_footer('data provided by Teleport api')
+        embed.set_thumbnail('https://www.plantemoran.com/-/media/images/insights-images/2018/04/thinking-about-becoming-a-smart-city.jpg?h=704&w=1100&la=en&hash=0F4F54BBECD3E501765A064202A24F8851D74E04')
+    else:
+        await ctx.send('invalid input :pensive:')
+
 @client.command(aliases = ['inv'])
 async def invite(ctx):
     await ctx.send('https://discord.com/api/oauth2/authorize?client_id=785077511758675988&permissions=0&scope=bot')
