45aebeee7e9661989a279db48bab33c455b0c4ce
delta7-138 <krishna.sharma@iiitb.org>
2021-04-08 14:06:38 +0530

    adding covid commands

--- bot.py
+++ bot.py
@@ -95,6 +95,7 @@ async def help(ctx):
     embed.add_field(name = "To get advice" , value = "h.adv" , inline = True)
     embed.add_field(name = "To get anime quote" , value = "h.aniq" , inline = True)
     embed.add_field(name = "To get Teleport City Summary" , value = "h.standardofliving <city name> aliases = h.sol" , inline = False)
+    embed.add_field(name = "Help for COVID info commands" , value = "h.cvhelp" , inline = False)
     embed.set_thumbnail(url = thumb)
     embed.set_footer(text = "requested by a busta")
     await ctx.send(embed = embed)
@@ -127,6 +128,19 @@ async def fmhelp(ctx):
     embed.add_field(name = "Command to see who knows a track" , value = "h.fmwkt `<artist> - <trackname>` (aliases = h.fmwt , h.fmwhoknowstrack)" , inline = False)
     embed.set_thumbnail(url = 'https://lh3.googleusercontent.com/proxy/BzW7U-yNC8RjUf2SWOEzDcRxlCjXZBx7RGjiGu7QdDm7g4aKHC3tE815KW-cyut1yBF-qOKhR0r5i919Fa2nPnYqITbp-bg4Rqs_dxE8b976G3bi9SMUIC88Qkw8RIOphMD7rrIsggvBzwtcZdwTSvqVVM-vzhdeQtc')
     embed.set_footer(text = 'requested by a busta')
+    await ctx.send(embed = embed)
+
+@client.command(aliases = ['cvh'])
+async def cvhelp(ctx):
+    member = ctx.message.author
+    name = member.name
+    if(member.nick!=None):
+        name = member.nick
+    embed = discord.Embed(title = "Hugo COVID commands help")
+    embed.add_field(name = "Command for deaths, recoveries and confirmed cases overall" , value = "h.cvw -d | -r | -c" , inline = False) 
+    embed.add_field(name = "Command for deaths,recoveries and confirmed cases by country" , value = "h.cvc <country> -d | -r | -c" , inline = False)
+    embed.set_footer(text = 'requsted by ' + str(name))
+    embed.set_thumbnail(url = 'https://www.statnews.com/wp-content/uploads/2020/02/Coronavirus-CDC-645x645.jpg')
     await ctx.send(embed = embed)
 
 @client.command(aliases = ['col'])
@@ -720,6 +734,117 @@ async def standardofliving(ctx , * , args):
             await ctx.send("City not available :pensive: :v:")
     else:
         await ctx.send('invalid input :pensive:')
+
+@client.command(aliases = ['cvw' , 'covidwld' , 'covw' , 'covidw'])
+async def covidworld(ctx , *args):
+    res = requests.get('https://covid-api.mmediagroup.fr/v1/cases')
+    content = json.loads(res.text)
+    param = ""
+    if(len(args)<1):
+        await ctx.send("Missin Arguments :nerd:")
+        return ;
+
+    if(args[0]=='-d'):
+        param = "deaths"
+    elif(args[0]=='-r'):
+        param = "recovered"
+    elif(args[0]=='-c'):
+        param = "confirmed"
+    else:
+        await ctx.send("Send appropriate argument :rage:")
+        return ;
+    
+    resDict = dict()
+    for key,value in content.items():
+        deaths = int(value["All"][param])
+        resDict[key] = deaths
+    
+    resDict = sorted(resDict.items(), key=lambda item: item[1] , reverse= True)
+    countries = list()
+    numbers = list()
+    for i in range(10):
+        countries.append(resDict[i][0])
+        numbers.append(resDict[i][1])
+    
+    fig, ax = plt.subplots(figsize =(16, 9))
+    ax.barh(countries, numbers)
+    ax.set_title("COVID " + param.upper()) 
+
+    for s in ['top', 'bottom', 'left', 'right']:
+        ax.spines[s].set_visible(False)
+    
+    ax.xaxis.set_ticks_position('none')
+    ax.yaxis.set_ticks_position('none')
+
+    ax.xaxis.set_tick_params(pad = 5)
+    ax.yaxis.set_tick_params(pad = 10)
+
+    for i in ax.patches:
+        ax.text(i.get_width()+0.2, i.get_y()+0.5, str(round((i.get_width()), 2)),fontsize = 20, fontweight ='bold',color ='grey')
+    buffer = BytesIO()
+    plt.savefig(buffer , format = "png")
+    buffer.seek(0)
+    plt.close()
+    fil = discord.File(filename = 'cvwd.png' , fp = buffer)
+    await ctx.send(file = fil)
+
+@client.command(aliases = ['cvc'])
+async def covidcountry(ctx , *args):
+    res = requests.get('https://covid-api.mmediagroup.fr/v1/cases')
+    content = json.loads(res.text)
+    country = args[0]
+    param = ""
+    try:
+        countrydata = content[country]
+        if(len(args)<2):
+            await ctx.send("Missin Arguments :nerd:")
+            return ;
+
+        if(args[1]=='-d'):
+            param = "deaths"
+        elif(args[1]=='-r'):
+            param = "recovered"
+        elif(args[1]=='-c'):
+            param = "confirmed"
+        else:
+            await ctx.send("Send appropriate argument :rage:")
+            return ;
+        
+        resDict = dict()
+        for key,value in countrydata.items():
+            tmp = int(value[param])
+            resDict[key] = tmp
+        
+        resDict = sorted(resDict.items(), key=lambda item: item[1] , reverse= True)
+        states = list()
+        numbers = list()
+        for i in range(10):
+            states.append(resDict[i][0])
+            numbers.append(resDict[i][1])
+        
+        fig, ax = plt.subplots(figsize =(16, 9))
+        ax.barh(states, numbers)
+        ax.set_title("COVID " + param.upper()) 
+
+        for s in ['top', 'bottom', 'left', 'right']:
+            ax.spines[s].set_visible(False)
+        
+        ax.xaxis.set_ticks_position('none')
+        ax.yaxis.set_ticks_position('none')
+
+        ax.xaxis.set_tick_params(pad = 5)
+        ax.yaxis.set_tick_params(pad = 10)
+
+        for i in ax.patches:
+            ax.text(i.get_width()+0.2, i.get_y()+0.5, str(round((i.get_width()), 2)),fontsize = 20, fontweight ='bold',color ='grey')
+        buffer = BytesIO()
+        plt.savefig(buffer , format = "png")
+        buffer.seek(0)
+        plt.close()
+        fil = discord.File(filename = 'cvc.png' , fp = buffer)
+        await ctx.send(file = fil)
+    except:
+        await ctx.send("Invalid Country name :rage:")
 
 @client.command(aliases = ['inv'])
 async def invite(ctx):
