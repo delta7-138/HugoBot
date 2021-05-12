51a6c3a8730b005bfc12d9452df8071916d8a61f
delta7-138 <krishna.sharma@iiitb.org>
2021-05-12 12:19:02 +0530

    adding codeforces cog

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ API_TOKEN = os.getenv('API_TOKEN')
 LAST_FM_TOKEN = os.getenv('LAST_FM_TOKEN')
 FIREBASE_URL = os.getenv('FIREBASE_URL')
 client = commands.Bot(command_prefix = 'h.')
-cogs = ["cogs.user" , "cogs.color"]
+cogs = ["cogs.user" , "cogs.color" , "cogs.codeforces"]
 for cog in cogs:
     client.load_extension(cog)
 client.remove_command('help')
@@ -511,58 +511,6 @@ async def covidcountry(ctx , *args):
         await ctx.send(file = fil)
     except:
         await ctx.send("Invalid Country name :rage:")
-
-@client.command(aliases = ['codefrand' , 'cfrand'])
-async def codeforcesrandomprob(ctx):
-    res = requests.get('http://codeforces.com/api/problemset.problems')
-    content = json.loads(res.text)
-    probNum = random.randint(0 , len(content["result"]["problems"]))
-    prob = content["result"]["problems"][probNum]
-    embed = discord.Embed(title = prob["name"] , url = "http://codeforces.com/problemset/problem/" + str(prob["contestId"]) + "/" + prob["index"] , description = 'Type : *' + prob["type"] + '*')
-    embed.add_field(name = "points" , value = prob["points"], inline = False)
-    embed.add_field(name = "rating" , value = prob["rating"], inline = False)
-    tmpstr = ""
-    for i in prob["tags"]:
-        tmpstr = tmpstr + i + "\n"
-    embed.add_field(name = "tags" , value = tmpstr)
-    embed.set_footer(text = "Random codeforces problem requested by a nerd")
-    embed.set_thumbnail(url = 'https://codeforces.com/predownloaded/3b/86/3b8616d876e29762202e93e184d4373eb62e7274.png')
-    await ctx.send(embed = embed)
-
-@client.command(aliases = ['cftag' , 'cft'])
-async def codeforcesprobwithtag(ctx , * , args):
-    tags = urllib.parse.urlencode({'tags' : args})
-    res = requests.get('http://codeforces.com/api/problemset.problems?' + tags)
-    content = json.loads(res.text)
-    if(len(content["result"]["problems"])==0):
-        await ctx.message.add_reaction("😠")
-        await ctx.send("Invalid tags :nerd:")
-    else:
-        probNum = random.randint(0 , len(content["result"]["problems"]))
-        prob = content["result"]["problems"][probNum]
-        points = "-"
-        if("points" in prob and prob["points"]!=None):
-            points = prob["points"]
-        embed = discord.Embed(title = prob["name"] , url = "http://codeforces.com/problemset/problem/" + str(prob["contestId"]) + "/" + prob["index"] , description = 'Type : *' + prob["type"] + '*' , color = 0xfffbff)
-        embed.add_field(name = "points" , value = points, inline = False)
-        embed.add_field(name = "rating" , value = prob["rating"], inline = False)
-        tmpstr = ""
-        for i in prob["tags"]:
-            tmpstr = tmpstr + i + "\n"
-        embed.add_field(name = "tags" , value = tmpstr)
-        embed.set_footer(text = "Random " + args + " problem requested by a nerd")
-        embed.set_thumbnail(url = 'https://codeforces.com/predownloaded/3b/86/3b8616d876e29762202e93e184d4373eb62e7274.png')
-        rating = int(prob["rating"])
-        emoji = ""
-        if(rating<=1200):
-            emoji = "🚮"
-        elif(rating<=2200):
-            emoji = "😈"
-        else:
-            emoji = "😨"
-        await ctx.message.add_reaction("✅")
-        msg = await ctx.send(embed = embed)
-        await msg.add_reaction(emoji)
 
 @client.command(aliases = ['ronq'])
 async def ronswansonquote(ctx):
