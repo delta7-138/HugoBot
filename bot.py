152474ac41f5e6defaa03caf95dcbd43942f634e
delta7-138 <krishna.sharma@iiitb.org>
2021-05-14 16:24:55 +0530

    quotes cog

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ FIREBASE_URL = os.getenv('FIREBASE_URL')
 intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(command_prefix = 'h.' , intents = intents)
-cogs = ['cogs.color' , 'cogs.codeforces' , 'cogs.randomfunc' , 'cogs.mars' , 'cogs.lastfm' , 'cogs.user']
+cogs = ['cogs.color' , 'cogs.codeforces' , 'cogs.randomfunc' , 'cogs.mars' , 'cogs.lastfm' , 'cogs.user' , 'cogs.quotes']
 
 for cog in cogs:
     client.load_extension(cog)
@@ -100,35 +100,6 @@ async def cvhelp(ctx):
     embed.set_thumbnail(url = 'https://www.statnews.com/wp-content/uploads/2020/02/Coronavirus-CDC-645x645.jpg')
     await ctx.send(embed = embed)
 
-
-#FM COMMANDS
-
-
-@client.command(aliases = ['adv'])
-async def advice(ctx):
-    member = ctx.message.author
-    uname = member.name
-    if(member.nick!=None):
-        uname = member.nick
-    res = requests.get('https://api.adviceslip.com/advice')
-    content = json.loads(res.text)
-    adviceMsg = content["slip"]["advice"]
-    embed = discord.Embed(title = "Advice for stupid people" , description = adviceMsg , color = 0x00ffea)
-    embed.set_thumbnail(url = 'https://i0.wp.com/www.brainpickings.org/wp-content/uploads/2014/10/nietzsche1.jpg?w=680&ssl=1')
-    embed.set_footer(text = 'requested by ' + uname)
-    await ctx.send(embed = embed)
-
-@client.command(aliases = ['aniq'])
-async def animequote(ctx):
-    res = requests.get('https://animechan.vercel.app/api/random')
-    content =json.loads(res.text)
-    quote = content["quote"]
-    character = content["character"]
-    anime = content["anime"]
-    embed = discord.Embed(title = "Anime Quote" , description = '*' + quote + '*'  , color = 0x00ffea)
-    embed.set_footer(text = 'by - ' + character + '\n from - ' + anime)
-    embed.set_thumbnail(url = 'https://i.pinimg.com/474x/89/08/61/8908616ffb91db2dbd1b640a374a1ee2.jpg')
-    await ctx.send(embed = embed)
 
 @client.command()
 async def urmom(ctx , member : discord.Member):
@@ -312,20 +283,6 @@ async def thispersondoesnotexist(ctx):
     fil = discord.File(filename = "ne.png" , fp = buffer)
     buffer.seek(0)
     await ctx.send(file = fil)
-
-@client.command(aliases = ['ronq'])
-async def ronswansonquote(ctx):
-    urls = ['https://hips.hearstapps.com/digitalspyuk.cdnds.net/17/18/1493816780-ron-swanson.jpg' , 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F13%2F2016%2F10%2F16%2FRon-Swanson-e1476642015275.jpg&q=85' , 'https://i.pinimg.com/originals/50/26/8f/50268f506228d83a5253f57225672c42.png']
-    member = ctx.message.author
-    name = member.name
-    if(member.nick!=None):
-        name = member.nick
-    res = requests.get("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
-    content = json.loads(res.text)
-    embed = discord.Embed(title = "Ron Swanson Quote" , description = content[0] , color = 0xff0000)
-    embed.set_thumbnail(url = urls[random.randint(0 , len(urls)-1)])
-    embed.set_footer(text = "requested by " + name)
-    await ctx.send(embed = embed)
 
 @client.command(aliases = ['stz'])
 async def settimezone(ctx , *args):
---
+++ cogs/quotes.py
@@ -0,0 +1,52 @@
+import discord
+import requests
+import json
+from discord.ext import commands
+
+class Quotes(commands.Cog):
+    def __init__(self , bot):
+        self.bot = bot
+
+    @commands.command(aliases = ['adv'])
+    async def advice(self , ctx):
+        member = ctx.message.author
+        uname = member.name
+        if(member.nick!=None):
+            uname = member.nick
+        res = requests.get('https://api.adviceslip.com/advice')
+        content = json.loads(res.text)
+        adviceMsg = content["slip"]["advice"]
+        embed = discord.Embed(title = "Advice for stupid people" , description = adviceMsg , color = 0x00ffea)
+        embed.set_thumbnail(url = 'https://i0.wp.com/www.brainpickings.org/wp-content/uploads/2014/10/nietzsche1.jpg?w=680&ssl=1')
+        embed.set_footer(text = 'requested by ' + uname)
+        await ctx.send(embed = embed)
+
+    @commands.command(aliases = ['aniq'])
+    async def animequote(self , ctx):
+        res = requests.get('https://animechan.vercel.app/api/random')
+        content =json.loads(res.text)
+        quote = content["quote"]
+        character = content["character"]
+        anime = content["anime"]
+        embed = discord.Embed(title = "Anime Quote" , description = '*' + quote + '*'  , color = 0x00ffea)
+        embed.set_footer(text = 'by - ' + character + '\n from - ' + anime)
+        embed.set_thumbnail(url = 'https://i.pinimg.com/474x/89/08/61/8908616ffb91db2dbd1b640a374a1ee2.jpg')
+        await ctx.send(embed = embed)
+
+    @commands.command(aliases = ['ronq'])
+    async def ronswansonquote(self , ctx):
+        urls = ['https://hips.hearstapps.com/digitalspyuk.cdnds.net/17/18/1493816780-ron-swanson.jpg' , 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F13%2F2016%2F10%2F16%2FRon-Swanson-e1476642015275.jpg&q=85' , 'https://i.pinimg.com/originals/50/26/8f/50268f506228d83a5253f57225672c42.png']
+        member = ctx.message.author
+        name = member.name
+        if(member.nick!=None):
+            name = member.nick
+        res = requests.get("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+        content = json.loads(res.text)
+        embed = discord.Embed(title = "Ron Swanson Quote" , description = content[0] , color = 0xff0000)
+        embed.set_thumbnail(url = urls[random.randint(0 , len(urls)-1)])
+        embed.set_footer(text = "requested by " + name)
+        await ctx.send(embed = embed)
+
+
+def setup(bot):
+    bot.add_cog(Quotes(bot))
