59679cb8aa8cd400a8db9e7c24b3b865315111a8
delta7-138 <krishna.sharma@iiitb.org>
2021-06-15 13:34:42 +0530

    user commands

--- cogs/user.py
+++ cogs/user.py
@@ -1,17 +1,10 @@
 import discord
 from discord.ext import commands
+from .image import ImageClass
 
 class User(commands.Cog):
-    def __init__(self , bot):
-        self.bot = bot
-
-    @commands.command(aliases = ['uh' , 'userh'])
-    async def userhelp(self , ctx):
-        embed = discord.Embed(title = "User Info Help" , color = 0xff00ff)
-        embed.add_field(name = "Get user avatar" , value = "h.av <mention member> or h.av" , inline = False)
-        embed.set_footer(text = "requested by " + ctx.message.author.name)
-        embed.set_thumbnail(url = "https://www.videogameschronicle.com/files/2021/05/discord-new-logo.jpg")
-        await ctx.reply(embed = embed , mention_author = True)
+    def __init__(self , client):
+        self.client = client
 
     @commands.command(aliases = ['av' , 'avtr'])
     async def avatar(self , ctx , member : discord.Member):
@@ -26,9 +19,18 @@ class User(commands.Cog):
             embed = discord.Embed(title = "Member avatar" , color = 0xff00ff)
             embed.set_image(url = member.avatar_url)
             await ctx.reply(embed = embed ,mention_author = True)
-            
+        
         if isinstance(err , commands.BadArgument):
-            await ctx.reply("Wrong argument" , mention_author = True)      
+            await ctx.reply("Wrong argument" , mention_author = True)
+
+    @commands.command(aliases = ['m' , 'avm' , 'mergeav'])
+    async def avatarmerge(self , ctx , member:discord.Member):
+        newobj = ImageClass()
+        await newobj.mergeImages(ctx.message.author.avatar_url , member.avatar_url)
+        fil = discord.File('out.png')
+        embed = discord.Embed(title = "Merged Avatars")
+        embed.set_image(url = 'attachment://out.png')
+        await ctx.reply(embed = embed , file = fil , mention_author = True)
 
 def setup(bot):
     bot.add_cog(User(bot))
