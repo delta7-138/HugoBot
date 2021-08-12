2f5693eb2e1635b46100eb28c22f0fc33ef3ffb8
delta7-138 <krishna.sharma@iiitb.org>
2021-08-12 11:06:40 +0530

    change

--- bot.py
+++ bot.py
@@ -103,14 +103,6 @@ async def cvhelp(ctx):
 
 
 
-@urmom.error
-async def urmomiserror(ctx , err):
-    if isinstance(err , commands.MissingRequiredArgument):
-        res = requests.get('https://api.yomomma.info/')
-        content = json.loads(res.text)
-        joke = content["joke"]
-        mention = ctx.message.author.mention
-        await ctx.send(mention + " " + joke)
 
 
 @client.command(aliases = ['tpdne' , 'ne'])
